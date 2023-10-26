e059d83d303828124510718cc2d570a9282f2483
Yeonju Jo <yjj0287@gmail.com>
2023-10-27 05:41:08 +0900

    💄 style: config 파일은 eslint 검사안하도록 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
