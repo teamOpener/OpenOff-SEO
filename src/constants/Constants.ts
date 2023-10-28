98f915fa0302acf6aba3d68f48f290b2a1175c8d
Yeonju Jo <yjj0287@gmail.com>
2023-10-28 11:30:24 +0900

    ✨ feat: 상수 정의

---
+++ src/constants/Constants.ts
@@ -0,0 +1,10 @@
+const CONST = {
+  YOU: "YOU'RE",
+  INVITED: 'INVITED',
+  JOIN_THE_EVENT: 'Join the event',
+  OFFICIAL_INSTA_ID: '@openoff_official',
+  COPYRIGHT: 'Copyright 2023. OPENER All Rights Reserved.',
+  OPEN_OFF: 'OPEN OFF',
+};
+
+export default CONST;
