0430a74333c14132da883f09c255bcb700a34c30
Yeonju Jo <yjj0287@gmail.com>
2023-10-28 12:03:58 +0900

    ✨ feat: footer 제작

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -3,6 +3,7 @@ const CONST = {
   INVITED: 'INVITED',
   JOIN_THE_EVENT: 'Join the event',
   OFFICIAL_INSTA_ID: '@openoff_official',
+  OFFICIAL_INSTA_LINK: 'https://www.instagram.com/openoff_official/',
   COPYRIGHT: 'Copyright 2023. OPENER All Rights Reserved.',
   OPEN_OFF: 'OPEN OFF',
 };
