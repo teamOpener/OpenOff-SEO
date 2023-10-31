041ce4b92b8207dd4b4ed9b776bbe72c1117b7b6
Yeonju Jo <yjj0287@gmail.com>
2023-10-31 20:29:26 +0900

    💅 design: 이미지 파일 정리

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,6 @@
+export { default as CardBottomLine } from './CardBottomLine.svg';
+export { default as CardRightLine } from './CardRightLine.svg';
+export { default as CardTopLine } from './CardTopLine.svg';
 export { default as IconDownload } from './IconDownload.svg';
 export { default as IconInsta } from './IconInsta.svg';
 export { default as IconShare } from './IconShare.svg';
